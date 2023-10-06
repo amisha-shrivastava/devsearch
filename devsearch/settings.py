47b1b2c1043267aec275908887d6d075047f6354
Amisha <amishashrivastava998@gmail.com>
2023-10-06 14:26:12 +0530

    styled pages

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r1@7m)d!9erq_%%$&+8mvi1p!9c*4*q=ck*9__-r)oxh&)6(rx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,13 @@ class ProjectForm(ModelForm):  #creates form based on the model
     class Meta:
         model = Project
         fields =  ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tag'] #includes all editable fields '__all__'
+        widgets = {
+            'tag': forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class':'input'})
+        # self.fields['description'].widget.attrs.update({'class':'input', 'placeholder': 'Add Description'})
+        
+    
