1ccc77cf1bcfbe6a424aef22635969aad630cb5f
Amisha <amishashrivastava998@gmail.com>
2023-10-08 22:33:43 +0530

    profile page updated

--- projects/urls.py
+++ projects/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects, name="projects"),
-    path('projects/<str:pk>', views.project, name="project"),
+    path('', views.projects, name="projects"),
+    path('<str:pk>', views.project, name="project"),
     path('create-project', views.createProject, name="create-project"),
     path('update-project/<str:pk>', views.updateProject, name="update-project"),
     path('delete-project/<str:pk>', views.deleteProject, name="delete-project"),
---
+++ users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User  #importing built in django user 
+import uuid
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True) #character field need max length
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png') #requires image processig library pillow by default image goes to static folder
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_website = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.user.username) #cant use anything except string
+    
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True) #character field need max length
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)    
+    
+    def __str__(self):
+        return str(self.name) #cant use anything except string
