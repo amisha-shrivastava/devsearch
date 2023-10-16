e34f9cf0504bdc5f94e00eb5e1f43d04bb95b825
Amisha <amishashrivastava998@gmail.com>
2023-10-16 13:46:01 +0530

    project and skills update function

--- projects/views.py
+++ projects/views.py
@@ -16,20 +16,25 @@ def project(request, pk):
 
 @login_required(login_url="login")
 def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
     if request.method == 'POST':
         #CREATION
         # print(request.POST)
+        
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.owner.save()
             return redirect('projects')
     context = {'form':form}
     return render(request, "projects/project-form.html",context)
 
 @login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id = pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id = pk)
     form = ProjectForm(instance = project)
     if request.method == 'POST':
         #CREATION
@@ -43,9 +48,11 @@ def updateProject(request, pk):
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id = pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
     context = {'object':project}
     return render(request,'projects/delete_template.html',context)
+
