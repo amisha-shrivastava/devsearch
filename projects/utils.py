256ba5ca65b14ded096c59167fbe9fe170cc2af0
Amisha <amishashrivastava998@gmail.com>
2023-10-18 22:09:20 +0530

    Pagination completed

--- projects/utils.py
+++ projects/utils.py
@@ -1,6 +1,30 @@
 from django.db.models import Q
 from . models import Project, Tag
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+
+
+def paginateProjects(request, projects, results):
+    page = request.GET.get('page')
+    paginator = Paginator(projects, results)
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        projects = paginator.page(paginator.num_pages)
+        
+    left_index = (int(page)-4)
+    if left_index<1:
+        left_index=1
+    right_index = (int(page)+5)
+    if right_index>paginator.num_pages:
+        right_index=paginator.num_pages
+        
+    custom_range = range(left_index,right_index+1)
+    return custom_range, projects
+    
 def searchProjects(request):
     search_query = ''
     if request.GET.get('search_query'):
